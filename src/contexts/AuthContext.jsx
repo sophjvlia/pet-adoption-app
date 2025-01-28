1c61d5838643bd04e3ef8dbaec21bccea362d1ea
sophjvlia <sophiejulianacrabtree@gmail.com>
2025-01-28 16:02:10 +0800

    Removed user_id upon logout

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -17,6 +17,7 @@ export const AuthProvider = ({ children }) => {
 
     const logout = () => {
         localStorage.removeItem('token');
+        localStorage.removeItem('user_id');
         setIsLoggedIn(false);
         setIsAdmin(false);
     };
