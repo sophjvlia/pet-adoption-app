3b349f45231a0c257dcfcad94963c5bb3ec1cd25
sophjvlia <sophiejulianacrabtree@gmail.com>
2025-01-28 16:01:12 +0800

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { BrowserRouter, Routes, Route, Outlet, useNavigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Outlet, useNavigate, Navigate } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import PetsListingPage from './pages/Pets'
@@ -8,15 +8,19 @@ import PetDetailsPage from './pages/PetDetails'
 import AdminPetsView from './pages/AdminPetsView'
 import AdminApplicationsView from './pages/AdminApplicationsView'
 import './App.css';
+import { useAuth } from './contexts/AuthContext';
 
-export function Layout({ isLoggedIn, setIsLoggedIn }) {
-  const navigate = useNavigate();
+export function Layout() {
+  const { isLoggedIn, isAdmin, logout } = useAuth();
+  console.log(isLoggedIn);
+  console.log(isAdmin);
+  // const navigate = useNavigate();
 
-  const handleLogout = () => {
-    localStorage.removeItem('token'); 
-    setIsLoggedIn(false); 
-    navigate('/login'); 
-  };
+  // const handleLogout = () => {
+  //   localStorage.removeItem('token'); 
+  //   setIsLoggedIn(false); 
+  //   navigate('/login'); 
+  // };
 
   return (
     <>
@@ -27,10 +31,10 @@ export function Layout({ isLoggedIn, setIsLoggedIn }) {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/pets">Pets</Nav.Link>
-              {isLoggedIn && <Nav.Link href="/dashboard/pets">Admin Pets</Nav.Link>}
-              {isLoggedIn && <Nav.Link href="/dashboard/applications">Admin Applications</Nav.Link>}
+              {isLoggedIn && isAdmin && <Nav.Link href="/dashboard/pets">Admin Pets</Nav.Link>}
+              {isLoggedIn && isAdmin && <Nav.Link href="/dashboard/applications">Admin Applications</Nav.Link>}
               {!isLoggedIn && <Nav.Link href="/login">Login</Nav.Link>}
-              {isLoggedIn && <Nav.Link onClick={handleLogout}>Logout</Nav.Link>}
+              {isLoggedIn && <Nav.Link onClick={logout}>Logout</Nav.Link>}
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -41,20 +45,20 @@ export function Layout({ isLoggedIn, setIsLoggedIn }) {
 }
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { isLoggedIn, isAdmin } = useAuth();
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    // Check if token exists in localStorage
-    const token = localStorage.getItem('token');
-    setIsLoggedIn(!!token);
-  }, []);
+  // useEffect(() => {
+  //   const token = localStorage.getItem('token');
+  //   setIsLoggedIn(!!token);
+  // }, []);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}>
+        <Route path="/" element={<Layout/>}>
           <Route path="signup" element={<Signup/>} />
-          <Route path="login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="login" element={<Login/>} />
           <Route path="pets" element={<PetsListingPage/>} />
           <Route path="pets/:id" element={<PetDetailsPage/>} />
           <Route path="dashboard/pets" element={isAdmin ? <AdminPetsView/> : <Navigate to="/"/>} />
